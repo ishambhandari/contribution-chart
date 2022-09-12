90761d2499ff5d0a5f6dcc8b70e8cd680702432d
Pratik Poudel <iampratik32@gmail.com>
2022-09-12 20:43:45 +0545

    Github Contribution Chart

--- src/Components/contribution-chart/index.js
+++ src/Components/contribution-chart/index.js
@@ -1,52 +1,36 @@
 import React, { useEffect, useState } from "react";
 
-const token = 'testing'
+const token = 'token'
 
 const headers = {
   "Content-Type": "application/json",
-  Authorization:`bearer ${token}` 
-};
+  Authorization: `bearer ${token}`
+}
+const github_baseurl = 'https://api.github.com/graphql';
 
-const query_github = `
-query {
-  viewer {
-    contributionsCollection(from: "2021-01-01T00:00:00", to: "2021-12-01T00:00:00") {
-      contributionCalendar {
-        weeks {
-          contributionDays {
-            contributionCount
-            date
+async function getData(url, username) {
+
+  const body = {
+    "query": `query {
+      user(login: "${username}") {
+        contributionsCollection(from: "2021-01-01T00:00:00", to: "2021-12-01T00:00:00") {
+          contributionCalendar {
+            weeks {
+              contributionDays {
+                contributionCount
+                date
+              }
+            }
           }
         }
       }
-    }
+    }`
   }
-}`
-const github_baseurl = 'https://api.github.com/graphql';
 
-async function getData(url) {
-  try {
-  const responseGitlab= fetch(github_baseurl, {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify({query:query_github})
-  })
-    // const responseGitlab = await axios.post(
-    //   `${url}`,
-    //   {
-    //     headers: {
-    //       "Authorization": "bearer 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V",
-    //     },
-    //   }
-    // );
-    responseGitlab.then((res)=>{
-    console.log("t", res)
-    return responseGitlab;
-    })
-  } catch (error) {
-    console.log("error", error);
-    return error;
-  }
+  const response = await fetch(github_baseurl,
+    { method: 'POST', body: JSON.stringify(body), headers: headers })
+  const data = await response.json()
+  console.log(data)
 }
 
 // https://gitlab.com/api/v4/events\?target_type\=issue\&action\=created\&after\=2021-01-31\&before\=2021-03-01\&scope\=all"
@@ -54,8 +38,8 @@ const ContributionChart = () => {
   const [gitlabdata, setGitlabdata] = useState(null);
 
   useEffect(() => {
-    getData(github_baseurl).then((res)=>
-    setGitlabdata(res)
+    getData(github_baseurl, "iampratik32").then((res) =>
+      setGitlabdata(res)
     )
   }, []);
   return (
